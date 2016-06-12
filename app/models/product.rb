f00450deb651e27559967b9a3850eae6a0d3c2a7
nickmwhite <nickmwhite@gmail.com>
2016-06-12 18:50:53 -0400

    added minimum title length and custom error message

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,8 @@ class Product < ActiveRecord::Base
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
-  validates :title, :description, :image_url, presence: true
+  validates :title, :description, :image_url, presence: true, length: { minimum: 10,
+                                                                        too_short: "must have at least 10 characters"}
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
